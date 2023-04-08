ace7f3624256c2697b96a64178a21ed4513f78d0
ryanmio <ryan@mioduski.us>
2023-04-08 17:46:03 -0400

    s

--- assets/js/script.js
+++ assets/js/script.js
@@ -72,7 +72,7 @@ document.getElementById("petition-form").addEventListener("submit", function (ev
     }),
   };
 
-  fetch("http://crm.littlefallsva.com/civicrm/extern/rest.php?entity=Contact&action=create", {
+  fetch("https://crm.littlefallsva.com/civicrm/extern/rest.php?entity=Contact&action=create", {
     method: "POST",
     headers: {
       "Content-Type": "application/x-www-form-urlencoded",
