e39e8c5e140d253a2a55578a1dd5f42301983a5f
ryanmio <ryan@mioduski.us>
2023-04-23 12:32:22 -0400

    Update subscription-form-function.js

--- netlify/functions/subscription-form-function/subscription-form-function.js
+++ netlify/functions/subscription-form-function/subscription-form-function.js
@@ -1,36 +1,46 @@
-const axios = require('axios');
-const querystring = require('querystring');
+const axios = require("axios");
+const querystring = require("querystring");
+
 exports.handler = async (event, context) => {
-  if (event.httpMethod !== 'POST') {
-    return { statusCode: 405, body: 'Method Not Allowed' };
+  if (event.httpMethod !== "POST") {
+    return { statusCode: 405, body: "Method Not Allowed" };
   }
-  console.log('event.body:', event.body);
+
+  console.log("event.body:", event.body);
   const data = querystring.parse(event.body);
 
-  const apiUrl = 'http://crm.littlefallsva.com/wp-content/plugins/civicrm/civicrm/extern/rest.php';
-  const apiKey = '8g497rFj6lomsmHHf2osBdWS';
-  const siteKey = 'ywPjjLTOwbzGf2kojonJBTBROiYlFSNKWxeAh48GTfE';
+  const apiUrl =
+    "http://crm.littlefallsva.com/wp-content/plugins/civicrm/civicrm/ajax/rest";
+  const apiKey = "8g497rFj6lomsmHHf2osBdWS";
+  const siteKey = "ywPjjLTOwbzGf2kojonJBTBROiYlFSNKWxeAh48GTfE";
 
   try {
-    console.log('Preparing to call the API...');
-    const params = {
-      entity: 'Contact',
-      action: 'create',
-      json: JSON.stringify({
-        sequential: 1,
-        contact_type: 'Individual',
-        email: data.email,
-      }),
-      api_key: apiKey,
-      key: siteKey,
-    };
-    const url = `${apiUrl}?${querystring.stringify(params)}`;
-    const response = await axios.post(url);
+    console.log("Preparing to call the API...");
 
-    console.log('API response:', response.data);
+    let response;
+    try {
+      response = await axios.get(apiUrl, {
+        params: {
+          entity: "Contact",
+          action: "create",
+          json: JSON.stringify({
+            sequential: 1,
+            contact_type: "Individual",
+            email: data.email,
+            api_key: apiKey,
+            key: siteKey,
+          }),
+        },
+      });
+    } catch (error) {
+      console.error("Error calling the API:", error.message);
+      return { statusCode: 500, body: `Error calling the API: ${error.message}` };
+    }
+
+    console.log("API response:", response.data);
 
     if (response.data.is_error === 0) {
-      return { statusCode: 200, body: 'Contact created successfully' };
+      return { statusCode: 200, body: "Contact created successfully" };
     } else {
       return {
         statusCode: 500,
