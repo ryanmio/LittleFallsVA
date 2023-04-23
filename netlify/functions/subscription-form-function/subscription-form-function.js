e4203fa72cf9d07c80bb77ef93e8ab9d254db79b
ryanmio <ryan@mioduski.us>
2023-04-23 12:12:02 -0400

    Update subscription-form-function.js

--- netlify/functions/subscription-form-function/subscription-form-function.js
+++ netlify/functions/subscription-form-function/subscription-form-function.js
@@ -4,21 +4,16 @@ exports.handler = async (event, context) => {
   if (event.httpMethod !== 'POST') {
     return { statusCode: 405, body: 'Method Not Allowed' };
   }
-console.log('event.body:', event.body);
-const data = querystring.parse(event.body);
-
-
+  console.log('event.body:', event.body);
+  const data = querystring.parse(event.body);
 
   const apiUrl = 'http://crm.littlefallsva.com/wp-content/plugins/civicrm/civicrm/extern/rest.php';
-  const apiKey = 'V3llrOygSJMujjCNQ8k9Q1px';
+  const apiKey = '8g497rFj6lomsmHHf2osBdWS';
   const siteKey = 'ywPjjLTOwbzGf2kojonJBTBROiYlFSNKWxeAh48GTfE';
 
-try {
-  console.log('Preparing to call the API...');
-  
-  let response;
   try {
-    response = await axios.post(apiUrl, querystring.stringify({
+    console.log('Preparing to call the API...');
+    const params = {
       entity: 'Contact',
       action: 'create',
       json: JSON.stringify({
@@ -28,18 +23,11 @@ try {
       }),
       api_key: apiKey,
       key: siteKey,
-    }), {
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-      },
-    });
-  } catch (error) {
-    console.error('Error calling the API:', error.message);
-    return { statusCode: 500, body: `Error calling the API: ${error.message}` };
-  }
+    };
+    const url = `${apiUrl}?${querystring.stringify(params)}`;
+    const response = await axios.post(url);
 
-  console.log('API response:', response.data);
-
+    console.log('API response:', response.data);
 
     if (response.data.is_error === 0) {
       return { statusCode: 200, body: 'Contact created successfully' };
