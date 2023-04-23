6aee9bf546198f08a2bdfce4a19979dd7757e3c0
ryanmio <ryan@mioduski.us>
2023-04-23 11:38:04 -0400

    Update subscription-form-function.js

--- netlify/functions/subscription-form-function/subscription-form-function.js
+++ netlify/functions/subscription-form-function/subscription-form-function.js
@@ -9,7 +9,7 @@ const data = querystring.parse(event.body);
 
 
 
-  const apiUrl = 'https://crm.littlefallsva.com/wp-content/plugins/civicrm/civicrm/extern/rest.php';
+  const apiUrl = 'http://crm.littlefallsva.com/wp-content/plugins/civicrm/civicrm/extern/rest.php';
   const apiKey = 'V3llrOygSJMujjCNQ8k9Q1px';
   const siteKey = 'ywPjjLTOwbzGf2kojonJBTBROiYlFSNKWxeAh48GTfE';
 
